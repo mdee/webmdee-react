ef08a08d15ce228f68b08bf5ad57a5aba6d5959c
Mark Dhillon <mark.dhillon@tosense.com>
2020-02-10 18:44:14 -0800

    Updates

---
+++ precache-manifest.599f3303e1330ea74a2579f8a821efd1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1849490198192ba4dd55bbf1f304945",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8323c90470468f9d1999",
+    "url": "/static/css/main.3391c9cd.chunk.css"
+  },
+  {
+    "revision": "e173006f0fac2bc21d95",
+    "url": "/static/js/2.3d75439f.chunk.js"
+  },
+  {
+    "revision": "8f4256b2e2d408626fa52cca92af72c9",
+    "url": "/static/js/2.3d75439f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8323c90470468f9d1999",
+    "url": "/static/js/main.ff103ed9.chunk.js"
+  },
+  {
+    "revision": "4dda5a6a828995817b28",
+    "url": "/static/js/runtime-main.beb2c1e9.js"
+  },
+  {
+    "revision": "9cc1d3949c66cd74a44de906c38ff115",
+    "url": "/static/media/campaign-data.9cc1d394.gif"
+  },
+  {
+    "revision": "62d467802f375fef3537c49a0d1e2a67",
+    "url": "/static/media/recording.62d46780.gif"
+  },
+  {
+    "revision": "e80803b481cdc2785be0ede2b342395e",
+    "url": "/static/media/reviewsage.e80803b4.gif"
+  }
+]);
