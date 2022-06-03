342f50af1da2cf074da610a34c5b7d654ac44a4f
dhillom1 <mark_dhillon@baxter.com>
2022-06-03 11:18:15 -0700

    Updates

---
+++ precache-manifest.9ea0f78792c0b1ced947a4e34b41c3ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "444b4d1c3378bf32e12f",
+    "url": "/static/css/main.7444de33.chunk.css"
+  },
+  {
+    "revision": "444b4d1c3378bf32e12f",
+    "url": "/static/js/main.444b4d1c.chunk.js"
+  },
+  {
+    "revision": "946fd4160512e06bc562",
+    "url": "/static/js/1.946fd416.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9cc1d3949c66cd74a44de906c38ff115",
+    "url": "/static/media/campaign-data.9cc1d394.gif"
+  },
+  {
+    "revision": "e80803b481cdc2785be0ede2b342395e",
+    "url": "/static/media/reviewsage.e80803b4.gif"
+  },
+  {
+    "revision": "62d467802f375fef3537c49a0d1e2a67",
+    "url": "/static/media/recording.62d46780.gif"
+  },
+  {
+    "revision": "ad3aa90ae30c68605e8466b9c35a48c4",
+    "url": "/index.html"
+  }
+];
